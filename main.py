29cc2da0597a8962ed2d1b07ceeb4f66ab0d9d72
Anas_Ibrahem <85556301+anas-1111@users.noreply.github.com>
2026-01-03 20:57:46 +0200

    Text analysis into a reusable function

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
-import re
-import string
-from collections import Counter
+def analyze_text(path):
+    import re
+    import string
+    from collections import Counter
 
-sentence_count = 0
-word_count = 0
-word_frequency = Counter()
+    sentence_count = 0
+    word_count = 0
+    word_frequency = Counter()
 
-with open("sample_text.txt", "r", encoding="utf-8") as file:
-    for line in file:
+    with open(path, "r", encoding="utf-8") as file:
+        for line in file:
+            line = line.lower()
+            sentence_count += len(re.findall(r'[.!?]', line))
+            clean_line = line.translate(str.maketrans("", "", string.punctuation))
+            words = clean_line.split()
+            word_count += len(words)
+            word_frequency.update(words)
 
-        line = line.lower()
+    return word_count, sentence_count, word_frequency
 
-        sentence_count += len(re.findall(r'[.!?]', line))
 
-        clean_line = line.translate(str.maketrans("", "", string.punctuation))
-        
-        word = clean_line.split()
-        word_count += len(word)
-        word_frequency.update(word)
+word_c, sentence_c, word_frequency = analyze_text("sample_text.txt")
 
-print("""
-Words count: {word_count}
-Sentences count: {sentence_count}
-Top 10 words:
-""".format(word_count=word_count, sentence_count=sentence_count))
+print("Words count: {}\nSentences count: {}\nTop 10 words: ".format(word_c, sentence_c))
 for word, count in word_frequency.most_common(10):
-    print(word, count)
+    print("{}: {}".format(word, count))
