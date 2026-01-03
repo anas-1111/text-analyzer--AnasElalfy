074650923df2a67fd9bb9d49185becd66357270d
Anas_Ibrahem <85556301+anas-1111@users.noreply.github.com>
2026-01-03 20:12:14 +0200

    sentence counter from re lib

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import re
+
+sentence_count = 0
+
+
 with open("sample_text.txt", "r", encoding="utf-8") as file:
-    for lines in file:
-        print(lines)
+    for line in file:
+        line = line.lower()
+        sentence_count += len(re.findall(r'[.!?]', line))
+
+print(sentence_count)
