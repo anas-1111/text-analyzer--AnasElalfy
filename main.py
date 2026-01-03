e240776dd346ae8f865201cf94e477b5ec53a6f5
Anas_Ibrahem <85556301+anas-1111@users.noreply.github.com>
2026-01-03 20:19:08 +0200

    Text cleaning remove special characters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import string
 
 sentence_count = 0
 
@@ -7,5 +8,7 @@ with open("sample_text.txt", "r", encoding="utf-8") as file:
     for line in file:
         line = line.lower()
         sentence_count += len(re.findall(r'[.!?]', line))
+        clean_line = line.translate(str.maketrans("", "", string.punctuation))
+        print(clean_line)
 
 print(sentence_count)
