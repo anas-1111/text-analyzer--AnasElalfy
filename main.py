80bdca99df7057bf73f9cca2f8560ec7561f1670
Anas_Ibrahem <85556301+anas-1111@users.noreply.github.com>
2026-01-03 20:22:59 +0200

    Words Counter

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import re
 import string
+from collections import Counter
 
 sentence_count = 0
-
+word_count = 0
+word_frequency = Counter()
 
 with open("sample_text.txt", "r", encoding="utf-8") as file:
     for line in file:
-        line = line.lower()
-        sentence_count += len(re.findall(r'[.!?]', line))
-        clean_line = line.translate(str.maketrans("", "", string.punctuation))
-        print(clean_line)
 
-print(sentence_count)
+        line = line.lower()
+
+        sentence_count += len(re.findall(r'[.!?]', line))
+
+        clean_line = line.translate(str.maketrans("", "", string.punctuation))
+        
+        word = clean_line.split()
+        word_count += len(word)
+        word_frequency.update(word)
+
